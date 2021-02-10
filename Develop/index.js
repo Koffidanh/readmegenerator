85c0079489bb283d64e8ea14c430aab82bf57a2f
KoffiDanhounsrou <koffi.danh@gmail.com>
2021-02-10 15:04:36 -0700

    array of questions created

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,12 @@
 const fs = require('fs');
 //require the inquirer module
 var inquirer = require('inquirer');
+//require the util module
+var util = require('util');
+//require the generator module
+var generatorMarkdown = require('./utils/generateMarkdown');
+
+
 
 // array of questions for user
 const questions = [
@@ -12,38 +18,67 @@ const questions = [
     },
     {
         type: 'confirm',
-        name: 'table of contents',
+        name: 'contents',
         message: 'Do you want to add Table of Contents?',
         answer: 'yes'
-        if(response[1] === answer) {
-            
-        }
-      },
-{
-    type: 'input',
+
+    },
+    {
+        type: 'input',
         name: 'description',
-            message: 'What is project description?',
-      },
-{
-    type: 'input',
+        message: 'What is project description?',
+    },
+    {
+        type: 'input',
         name: 'installation',
-            message: 'Provide a step-by-step description of how to get the application running?',
-      },
-{
-    type: 'input',
+        message: 'Provide a step-by-step description of how to get the application running?',
+    },
+    {
+        type: 'input',
         name: 'usage',
-            message: 'Provide instructions and examples for use including screenshots',
+        message: 'Provide instructions and examples for use including screenshots',
+    },
+    {
+
+        type: 'input',
+        name: 'collaborators',
+        message: 'Provide name of the collaborators. ',
+        
+    },
+    {
+
+        type: 'input',
+        name: 'github',
+        message: 'What is the collaborator GitHub username? ',
+        
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for the project.',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0'],
+        
       },
+
 ];
+inquirer.prompt(questions).then((data) => {
+    console.log('\nUser input:');
+    console.log(JSON.stringify(data, null, '  '));
+   
+});
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
+// // function to write README file
+function writeToFile('README.md', generatorMarkdown(data) , (err) =>
+  
+err ? console.error(err) : console.log('Readme created!')
+);
 
-}
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ![badge](https://img.shields.io/badge/license-${data.licenses}-brightgreen)<br />
+ ## The Repository
+ ${data.description}
+ ## ${data.contents} {
+   ## Table of Contents
+   ### [Installation](#installation)
+   ### [Usage](#usage)
+   ### [Credits](#credits)
+   ### [License](#license)
+ },
+ ## Installation
+ ${data.installation}
+ ## Usage
+ ${data.usage}
+ ## Contribution
+ ${data.collaborators}
 
+ ## License
+ Licensed under the ${data.license}.
+  
 `;
 }
 
